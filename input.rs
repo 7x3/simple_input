a81456cc8622389e198484ffe04062d7ee3acbcc
7x3 <JJWarrell@gmail.com>
2020-12-30 17:07:46 -0800

    Update input.rs

--- input.rs
+++ input.rs
@@ -1,10 +1,7 @@
 pub mod input {
     pub fn readline() -> String {
         let mut line = String::new();
-        //println!("Enter your name :");
         let b1 = std::io::stdin().read_line(&mut line).unwrap();
-        //println!("Hello , {}", line);
-        //println!("no of bytes read , {}", b1);
         return line;
     }
 }
